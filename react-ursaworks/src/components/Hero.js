b71d67467337ef44441ae99d5f5d53ec10320b86
mxu27 <mxujoy@gmail.com>
2025-08-23 21:21:07 -0500

    change of club name

--- react-ursaworks/src/components/Hero.js
+++ react-ursaworks/src/components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
             {/* <img src={BearBg} className="heroBg" alt="Background" /> */}
             <div className="heroContent">
                 <img src={stars} className="stars" alt="Stars" />
-                <h1 className="title">ROBOMASTERS</h1>
+                <h1 className="title">URSAWORKS ROBOMASTER CLUB</h1>
                 <h2 className="subtitle">From Washington University in St.Louis</h2>
             </div>
         </div>
